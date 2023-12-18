4c7cbf256a48d714d639c03b1a70b674bb0204c6
zhuchaoran <1151084234@qq.com>
2023-12-18 17:29:06 +0800

    提交项目模版

---
+++ chatbot-api-interfaces/src/main/java/com/zcr/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.zcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : zhuchaoran
+ * @description: 启动入口
+ * @date : 2023/12/18
+ */
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+         SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/com/zcr/test/ApiTest.java
@@ -0,0 +1,9 @@
+package com.zcr.test;
+
+/**
+ * @author : zhuchaoran
+ * @description: 测试
+ * @date : 2023/12/18
+ */
+public class ApiTest {
+}
